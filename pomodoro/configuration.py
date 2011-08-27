2a9e65b0a1bf0ebbb15cc5acfeb6ce8b484c53d1
Marcos Alejandro Vanetta <marcosvanetta@gmail.com>
2011-08-27 18:28:03 -0300

    preparing setup.py

--- pomodoro/configuration.py
+++ pomodoro/configuration.py
@@ -36,7 +36,7 @@ def base_directory():
 
 def icon_directory():
     """Returns the location of the icons"""
-    return os.path.dirname(os.path.realpath(__file__)) + os.path.sep + "../images" + os.path.sep
+    return os.path.dirname(os.path.realpath(__file__)) + os.path.sep + "./images" + os.path.sep
 
 if __name__ == "__main__":
     print __doc__
